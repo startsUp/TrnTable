3d53c7397b6002bab9996399f070950e43bc2c39
shardool <pates25@mcmaster.ca>
2018-12-12 16:34:03 -0500

    input field changes and dashboard ui

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
 import '../App.css'
 
+
+const Track = props => (
+    // url, albumArt.url, albumName, artists
+    <div className='track-card-container'>
+        <div className='track-art'>
+            <img src={props.albumArtURL}/>
+        </div>
+        <div className='track-info'>
+            <div id='track-name' className='track-info-text'>{props.trackName}</div>
+            <div id='track-artists' className='track-info-text'>{props.trackArtists}</div>
+        </div>
+    </div>
+     
+)
+
+const TrackQueue = props => {
+    return(<div></div>)
+
+}
+
 class Dashboard extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            queue : [{trackName: Kids, trackArtists: [MGMT], albumArt: ''}]
+        }
+    }
     render() {
     return (
 		<div className='dashboard-container'>
+            <div className='dashboard-header'>
+                 
+            </div>
+            <div className='dashboard-queue-container'>
+                
+            </div>
 		</div> 
     )
 	}
--- src/pages/session.js
+++ src/pages/session.js
@@ -11,62 +11,59 @@ const Divider = props => {
     return(<div className='divider'/>)
 }
 
-const RoomCodeCard = props => {
-    return(
-        <div className='room-card'>
-            <div id='room-code-card'>
-                <div><img src={props.icon}/></div>
-                <div className='room-card-title'>
-                    {props.cardTitle}
-                </div>
-                {props.id==='host' ?
-                    <input id='room-code-input' value={props.roomCode} readOnly/>:
-                    <input id='room-code-input' maxLength='4'/>
-                }
-                <div id='room-text-tip'> {props.cardInfo}</div>
-                <a id='continue-button' className='button-container' onClick={props.onClick}>
-					<span id='spotify-button'>
-						Let's Get This Party Started
-					</span>
-				</a>
-               
+const RoomCodeCard = props => (
+    <div className='room-card'>
+        <div id='room-code-card'>
+            <div><img src={props.icon}/></div>
+            <div className='room-card-title'>
+                {props.cardTitle}
             </div>
+            {props.id==='host' ?
+                <input className='room-code-input' value={props.roomCode} readOnly/>:
+                <input id={props.inputID? '':'input-invalid'} className='room-code-input'  onKeyPress={props.onEnter} maxLength='4'/>
+            }
+            <div id='room-text-tip'> {props.cardInfo}</div>
+            <a id='continue-button' className='button-container' onClick={props.onClick}>
+                <span id='spotify-button'>
+                    Let's Get This Party Started
+                </span>
+            </a>
+            
         </div>
-    )
-}
-const SessionCards = props => {
-    return (
-        <div className='session-types'>
-                <div className='session-types-card' onClick={() => props.pickSession('host')}>
-                    <div className='card-title'>
-                        Host a session
-                    </div>
-                    <Divider/>
-                    <div className='card-info'>
-                        Your crew can join a room and request some bumpin' music!
-                    </div>
-                    <div><img src={djIcon}/></div>
+    </div>
+)
+const SessionCards = props => (
+    <div className='session-types'>
+            <div className='session-types-card' onClick={() => props.pickSession('host')}>
+                <div className='card-title'>
+                    Host a session
                 </div>
-                <div className='session-types-card' onClick={() => props.pickSession('join')}>
-                    <div className='card-title'>
-                        Join a session
-                    </div>
-                    <Divider/>
-                    <div className='card-info'>
-                        Join your crew and request some bumpin' music to liven up the party!   
-                    </div>
-                    <div><img src={speakerIcon}/></div>
+                <Divider/>
+                <div className='card-info'>
+                    Your crew can join a room and request some bumpin' music!
+                </div>
+                <div><img src={djIcon}/></div>
+            </div>
+            <div className='session-types-card' onClick={() => props.pickSession('join')}>
+                <div className='card-title'>
+                    Join a session
+                </div>
+                <Divider/>
+                <div className='card-info'>
+                    Join your crew and request some bumpin' music to liven up the party!   
+                </div>
+                <div><img src={speakerIcon}/></div>
 
-                </div>
-            </div> 
-    )
-}
+            </div>
+        </div> 
+)
 class SessionType extends Component {
     constructor(){
         super()
         this.state = {
             sessionType: null, 
             roomCode: null,
+            isInputValid: true,
         }
     }
     
@@ -90,6 +87,17 @@ class SessionType extends Component {
         //update state with room code
     }
 
+    validateInput = (input) => {
+        if (input.length === 4) this.enterRoom(input)
+        else this.setState({isInputValid: false})
+    }
+    enterRoom = (roomCode = this.state.roomCode) => {
+        console.log(roomCode)
+    }
+
+    inputSubmit = (e) => {
+        if (e.key === 'Enter') this.validateInput(e.target.value)
+    }
     render() {
         var card = null
         var headerInfo = null
@@ -99,7 +107,8 @@ class SessionType extends Component {
             var info = this.state.sessionType==='host' ? 'Give this code to your friends to join your session.' : 'You can get this code from the host.'
             var buttonText = this.state.sessionType==='host' ? 'Start' : 'Join'
             card = <RoomCodeCard id={this.state.sessionType} roomCode={this.state.roomCode}  
-                    cardTitle={title} cardInfo={info} icon={icon} buttonText={buttonText}/>
+                    cardTitle={title} cardInfo={info} icon={icon} buttonText={buttonText} 
+                    onEnter={this.inputSubmit} inputID={this.state.isInputValid} onClick={this.enterRoom}/>
         }
             
         else{
