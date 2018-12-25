af36ec030ac28e4fb905b948e4325b2cb8088385
shardool <pates25@mcmaster.ca>
2018-12-24 19:20:46 -0500

    add album, playlist api request and renders

--- src/pages/components/list.js
+++ src/pages/components/list.js
@@ -5,10 +5,10 @@ import {ReactComponent as AddedIcon} from '../../res/images/added.svg'
 
 const ListItem = props => (
     <div className='list-item'>
-        <div className='list-icon'>
-            <img src={props.item.iconURL}/>
+        <div className='list-image'>
+            <img className='list-item-image' src={props.item.iconURL}/>
         </div>
-        <div className='list-content-container'>
+        <div className='list-content-container' onClick={props.contentClick}>
             <div className='list-content'>
                 {props.item.content}   
             </div>
@@ -34,35 +34,38 @@ class List extends Component {
         selected : [], 
     }
     handleToggle = value => {
-        const { selected } = this.state;
-        const currentIndex = selected.indexOf(value);
-        const newSelected = [...selected];
+        const { selected } = this.state
+        const currentIndex = selected.indexOf(value)
+        const newSelected = [...selected]
     
         if (currentIndex === -1) {
-          newSelected.push(value);
+          newSelected.push(value)
         } else {
-          newSelected.splice(currentIndex, 1);
+          newSelected.splice(currentIndex, 1)
         }
     
         this.setState({
           selected: newSelected,
-        });
+        })
+        this.props.updateTracks(newSelected)
     }
     render() {
         var list = null
-        if(this.props.items){
+        if(this.props.items.length > 0){
             list = this.props.items.map((item, value) => {
                 var selected = this.state.selected.indexOf(value) !== -1 
                 return(<ListItem key={item.id} 
                                  item={item} 
                                  type={this.props.type}
                                  selected={selected}
-                                 onToggle={() => this.handleToggle(value)}/>)
+                                 onToggle={() => this.handleToggle(value)}
+                                 contentClick={this.props.contentClick}/>)
             })
         }
         const emptyMessage = <div className='room-text-tip'>
                                 {this.props.emptyMessage}
                             </div>
+       
        
         return (
             <div className='list-container'>
--- src/pages/trackImport.js
+++ src/pages/trackImport.js
@@ -3,6 +3,7 @@ import '../App.css'
 import appIcon from '../res/images/logo.webp'
 import List from './components/list'
 import {ReactComponent as MenuIcon} from '../res/images/menu.svg'
+import placeholderIcon from '../res/images/spotifyIcon.png'
 //A SearchResultSection can be used to show track, album and artist results
 const SearchResultSection = props => (
     <div>
@@ -14,7 +15,7 @@ const Divider = props => {
 }
 
 const UserLibraryButton = props => (
-    <div className='import-library-button' onClick={()=> props.showLibrary(props.id)}>
+    <div className='import-library-button' id={props.selected ? 'active':''} onClick={()=> props.showLibrary(props.id)}>
         <div>{props.icon}</div>
         <div>{props.desc}</div>
     </div>
@@ -23,9 +24,9 @@ const LibrarySidebar = props => (
     <div className='import-library-container'>
         <div className='heading' id='import-head'> Your Library </div>
         <Divider/>
-        <UserLibraryButton id='songs' desc='Songs' showLibrary={props.showLibrary}/>
-        <UserLibraryButton id='playlst' desc='Playlist' showLibrary={props.showLibrary}/>
-        <UserLibraryButton id='albums' desc='Albums' showLibrary={props.showLibrary}/>
+        <UserLibraryButton id='songs' desc='Songs' selected={'songs'===props.view} showLibrary={() => props.showLibrary('songs')}/>
+        <UserLibraryButton id='playlst' desc='Playlist' selected={'playlist'===props.view} showLibrary={() => props.showLibrary('playlist')}/>
+        <UserLibraryButton id='albums' desc='Albums' selected={'albums'===props.view}  showLibrary={() => props.showLibrary('albums')}/>
     </div>
 )
 const ImportContainer = props => (
@@ -37,8 +38,9 @@ const ImportContainer = props => (
         </div>
         <Divider color='#1ED660' width='100%' height='0.05em'/>
         <div className='import-main-container' id={props.showSidebar ? 'import-w-sidebar': ''}>
-            {props.showSidebar && <LibrarySidebar showLibrary={props.showLibrary} />} 
-            <List emptyMessage={props.emptyMessage} items={props.list} type={props.view} onAction={props.onAction}/>
+            {props.showSidebar && <LibrarySidebar showLibrary={props.showLibrary} view={props.view} />} 
+            <List emptyMessage={props.emptyMessage} items={props.list} 
+                  type={props.view} onAction={props.onAction} updateTracks={props.updateTracks}/>
         </div>
         
         
@@ -56,6 +58,7 @@ class ImportTrack extends Component {
         this.state = {
             queue: [], //actual queue that gets updated when the user chooses to see it
             userSavedSongs: [],
+            selectedSavedSongs: [],
             savedSongsOffset: 0,
             userPlaylists: [],
             playlistOffset: 0,
@@ -64,23 +67,21 @@ class ImportTrack extends Component {
             albumOffset: 0,
             tracksToAdd : [], //cache 
             selectedPlaylist : null,
-            showingQueue: true,
+            showingQueue: false,
             view: 'songs',
             showSidebar: true
         }
     }
     componentDidMount = () => {
-        this.props.apiRef.getMySavedTracks()
+        this.props.apiRef.getMySavedTracks({limit:50})
             .then(data => {
-                this.setState({userSavedSongs: this.parseData('songs', data.items)})
+                this.setState({userSavedSongs: this.parseData('songs', data.items), savedSongsOffset:50})
             })
-        this.setState({savedSongsOffset: 20})
     }
 
     parseData = (dataType, data) => {
         if(dataType === 'songs')
         {
-            console.log(data)
             var tracks = []
             data.forEach(item => { 
                 var track = item.track  
@@ -97,26 +98,65 @@ class ImportTrack extends Component {
             })
             return tracks
         }
-    }
+        else if (dataType === 'playlist')
+        {
+            var playlists = []
+            data.forEach(item => { 
+                playlists.push({name: item.name,
+                             content: item.name,
+                             iconURL: item.images.length > 0 ? item.images[0].url : placeholderIcon, 
+                             id: item.id, 
+                             uri: item.uri})
+            })
+            return playlists
+        }
+        else if (dataType === 'albums')
+        {
+            var albums = []
+            data.forEach(item => { 
+                var album = item.album
+                var artists = album.artists.map(elem => {return elem.name}).join(", ")
+                albums.push({name: album.name,
+                             artists: artists,
+                             content: album.name,
+                             subContent: artists,
+                             iconURL: album.images[2].url,
+                             id: album.id,
+                             totalTracks: album.total_tracks, 
+                             uri: album.uri})
+            })
+    
+            return albums
+        }
 
+    }
+    updateTracks = (tracks) => {
+        this.setState({tracksToAdd: tracks})
+    }
     listChange = (change, id, obj = null) => {
         
     }
     showLibrary = (type) => {
-        if(type === 'songs' && this.state.savedSongsOffset === 0){
-            this.props.apiRef.getMySavedTracks()
-                             .then(data => console.log(data))
-            this.setState({savedSongsOffset: 20})
+        if(type === 'songs'){
+            this.props.apiRef.getMySavedTracks({offset: this.state.savedSongsOffset, limit:50})
+                .then(data =>
+                    this.setState({userSavedSongs: this.parseData(type, data.items), 
+                                   savedSongsOffset: this.savedSongsOffset+50, view: 'songs'}
+                    ))
         }
-        else if (type === 'playlist' && this.state.playlistOffset === 0){
-            this.props.apiRef.getUserPlaylists()
-                             .then((data) => console.log(data))
-            this.setState({playlistOffset: 20})
+        else if (type === 'playlist'){
+            this.props.apiRef.getUserPlaylists({offset: this.state.playlistOffset, limit:50})
+                             .then(data =>
+                                this.setState({userPlaylists: this.parseData(type, data.items), 
+                                               playlistOffset: this.playlistOffset+50, view:'playlist'}
+                                ))
         }
-        else if (this.state.albumOffset === 0) { //albums
-            this.props.apiRef.getMySavedAlbums()
-                             .then((data) => console.log(data))
-            this.setState({albumOffset: 20})
+        else if (type === 'albums') { //albums
+            this.props.apiRef.getMySavedAlbums({offset: this.state.albumOffset, limit:50})
+                             .then(data =>
+                                this.setState({userAlbums: this.parseData(type, data.items), 
+                                               albumOffset: this.albumOffset+50, view:'albums'}
+                                ))
         }
     }
     moveTracksToQueue = () => {
@@ -137,16 +177,17 @@ class ImportTrack extends Component {
         const view = this.state.view
         var emptyMessage = view === 'queue' ? 
                             'Added songs will show up here' : 'No saved ' + this.state.view
-        var list = null
+        var list = []
         if (view === 'queue') 
             list = this.state.tracksToAdd
         else if(view === 'songs') 
             list = this.state.userSavedSongs
         else if(view === 'playlist')
             list = this.state.userPlaylists
-        else 
+        else if (view === 'albums')
             list = this.state.userAlbums
         
+        console.log(this.state.userAlbums)
         return (
             <div className='session-container' id='import'>
                 <div className='session-header'>
@@ -164,7 +205,8 @@ class ImportTrack extends Component {
                                 view={view}
                                 showSidebar={this.state.showSidebar}
                                 toggleSidebar={this.toggleSidebar}
-                                onAction={this.listChange}/>
+                                onAction={this.listChange}
+                                updateTracks={this.updateTracks}/>
             </div> 
         )
 	}
