4173d464714b9a3d37ad675f7a8ee3a5749c56cf
shardool <shardool_patel@hotmail.com>
2019-01-31 23:15:12 -0500

    fix svg pattern

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
 import '../App.css'
 import AppLogo from '../components/logo'
+import ConfirmActionPopup from '../components/confirmPopup'
+import {ReactComponent as InfoLogo} from '../res/images/round-info.svg'
 class Login extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            popup: {show: false}
+        }
+    }
+    handleClick = () => {
+        const popup = {show: true,  
+            title: 'Whats TrnTable?', 
+            accept: 'Cool!',
+            onAccept: this.handleClose,
+            message: <div>A web app that lets spotify users create a party playlist and host sessions where other users can upvote, downvote and request songs to be added. Made by <a href='https://github.com/startsUp'>Shardool Patel</a></div>, 
+            close: this.handleClose,
+          }
+        
+        this.setState({popup: popup})
+    }
+    handleClose = () => {
+        this.setState({popup: {show: false}})
+    }
     render() {
+        const {popup} = this.state
     return (
 		<div className='login-page-container'>
+            {popup.show && <ConfirmActionPopup popupInfo={popup}/>}
+            <InfoLogo className='dash-logo' id='info-icon' onClick={this.handleClick}/>
 			<div className='login-page-logo'>
 				<AppLogo styleName='app-logo' animate={true}/>
 				<div className='login-page-title'>TrnTable</div>
